0131e1a52904c297559936cfe438fa2f19dea1a1
Quentin Champenois <quentinchampenois@MacBook-Pro-de-Quentin.local>
2019-07-22 22:20:32 +0200

    fix fermeture du programme

--- client/main.go
+++ client/main.go
@@ -15,15 +15,16 @@ type Message struct {
 func main() {
     // Définir un nouveau listener sur l'entrée standard
     s := bufio.NewScanner(os.Stdin)
+    fmt.Println("Veuillez écrire votre message :")
 
     for s.Scan() {
-        fmt.Println("Veuillez écrire votre message : \n")
         // Définir la sortie d'ecriture sur os.Stdout
         e := json.NewEncoder(os.Stdout)
         // On enregistre le message dans le Message.Body
         m := Message{Body:s.Text()}
-        // Encoder m 
+        // Encoder m
         e.Encode(m)
+        break;
     }
     if err := s.Err(); err != nil {
         log.Fatal(err)
