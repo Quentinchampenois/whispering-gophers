e60a959fc818bd4dc3bd61452b33db428177cbd0
Quentin Champenois <quentinchampenois@MacBook-Pro-de-Quentin.local>
2019-07-25 16:16:10 +0200

    Step 5 ok

--- client/main.go
+++ client/main.go
@@ -1,5 +1,7 @@
 package main
 
+//  Chaque utilisateur à un serveur de réception et communique sur le serveur de l'autre
+
 import (
     "fmt"
     "os"
@@ -9,6 +11,7 @@ import (
     "flag"
     "net"
     "io"
+    "github.com/campoy/whispering-gophers/util"
 )
 
 const (
@@ -16,45 +19,87 @@ const (
     PORT = "3000"
     METHOD = "tcp"
 )
-// Définir un flag "-addr" dont la valeur par défaut est "localhost:3000"
-var addr = flag.String("addr", HOST+":"+PORT, "host:port - Connect to server")
 
+// Définir un flag "-addr" dont la valeur par défaut est "localhost:3000"
+var (
+    addr = flag.String("addr", HOST+":"+PORT, "host:port - Connect to server")
+	dialAddr   = flag.String("dial", HOST+":3001", "host:port listener server")
+    self string;
+)
 type Message struct {
+    Addr string
     Body string
 }
 
 func main() {
-
     flag.Parse()
-    fmt.Println(*addr)
 
-    // Définir un nouveau listener sur l'entrée standard
-    s := bufio.NewScanner(os.Stdin)
-    fmt.Println("Veuillez écrire votre message :")
 
-    c, err := net.Dial(METHOD, *addr)
-
-    m := Message{}
-    for s.Scan() {
-        // Définir la sortie d'ecriture sur os.Stdout
-        e := json.NewEncoder(c)
-        // On enregistre le message dans le Message.Body
-        m.Body = s.Text()
-        // Encoder m
-        e.Encode(m)
-        break;
-    }
-    if err := s.Err(); err != nil {
-        log.Fatal(err)
-    }
-
+    // New server
+    server, err := util.Listen()
     if err != nil {
-        log.Fatal(err)
+        log.Println(err)
+        return
     }
-    //c.Write(json.March)
+    self = server.Addr().String()
+    log.Println("Listening on ", self)
 
+    go dial(*dialAddr, self)
+    defer server.Close()
+    // Infinite loop waiting for user connection
+    for {
+        conn, err := server.Accept()
+        if err != nil {
+            log.Println(err)
+            return
+        }
 
-    io.Copy(os.Stdout, c)
-    c.Close()
-    fmt.Println("okokok")
+        go request(conn, server.Addr().String())
+    }
+}
+
+func request(conn net.Conn, lAddr string) {
+    defer conn.Close()
+
+    decoder := json.NewDecoder(conn)
+    m := Message{}
+    for {
+        if err := decoder.Decode(&m); err != nil {
+            log.Println(err)
+            return
+        }
+        fmt.Println("Message reçu !")
+        fmt.Println(m.Addr)
+        fmt.Println(m.Body)
+
+    }
+
+    io.Copy(conn, conn)
+    dial(m.Addr, lAddr)
+    conn.Close()
+}
+
+func dial(addr, lAddr string) {
+	c, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Println(err)
+        return
+	}
+
+fmt.Println(lAddr)
+	s := bufio.NewScanner(os.Stdin)
+	e := json.NewEncoder(c)
+    m := Message{Addr: lAddr}
+	for s.Scan() {
+		m.Body = s.Text()
+		err := e.Encode(m)
+		if err != nil {
+			log.Println(err)
+            return
+		}
+	}
+	if err := s.Err(); err != nil {
+		log.Println(err)
+        return
+	}
 }
